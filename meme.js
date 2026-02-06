0c2ecfce2dfaee8cdf6ce1ccbd12f577a831e78a
Kamal Kumar <chauhankamal591@gmail.com>
2026-02-06 07:33:33 +0530

    Add files via upload

---
+++ meme.js
@@ -0,0 +1,21 @@
+const generateBtn = document.querySelector('.generate-button')
+const memeTitle = document.querySelector('.meme-title')
+const memeImage = document.querySelector('.meme-image')
+const authorOutput = document.querySelector('.author')
+
+function getMeme (){
+    fetch('https://meme-api.com/gimme/wholesomememes')
+    .then((res) => res.json())
+    .then((data) => {
+     const {author, title, url} = data
+     memeTitle.innerHTML = title
+     memeImage.src = url
+     authorOutput.innerHTML = `Meme by: ${author}`
+    })
+}
+
+getMeme()
+
+generateBtn.addEventListener('click', () => {
+    getMeme()
+})
